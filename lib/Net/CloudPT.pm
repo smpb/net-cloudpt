78f52dd51d17291e419a6b1af04b55835b5bd015
Sérgio Bernardino <code@sergiobernardino.net>
2013-03-10 22:51:55 +0000

    Minor module API changes. Expanded the module's documentation.

--- lib/Net/CloudPT.pm
+++ lib/Net/CloudPT.pm
@@ -11,28 +11,85 @@ use LWP::Protocol::https;
 use HTTP::Request::Common;
 use Data::Random 'rand_chars';
 
+=head1 NAME
+
+Net::CloudPT - A CloudPT interface
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
 our $VERSION = '0.01';
 
+=head1 SYNOPSYS
 
-sub _nonce { join( '', rand_chars( size => 16, set => 'alphanumeric' )); }
+This module is a Perl interface to the API for the Portuguese cloud storage
+service CloudPT. You can learn more about it at L<http://www.cloudpt.pt>.
+
+Quick start:
+
+    use Net::CloudPT;
+
+    my $cloud = Net::CloudPT->new( key => 'KEY', secret => 'SECRET' );
+    $cloud->login;
+
+    # authorize the app, retrieving the verifier PIN...
+
+    $cloud->authorize( verifier => $pin );
+
+The particular details regarding the API can be found at L<https://cloudpt.pt/documentation>
+
+=head1 API
+
+=head2 new
+
+Create a new C<Net::CloudPT> object. The C<key> and C<secret> parameters are required.
+
+=cut
 
 sub new
 {
   my $class = shift;
 
-  my %args = @_;
   my $self = bless {
-    map { lc $_ => $args{$_} } keys %args
+    $class->_normalize_args( @_ )
   }, $class;
 
-  $self->{ua} ||= LWP::UserAgent->new;
-  $self->{callback_url}   ||= 'oob',
-  $self->{request_url}    ||= 'https://cloudpt.pt/oauth/request_token',
-  $self->{authorize_url}  ||= 'https://cloudpt.pt/oauth/authorize',
-  $self->{access_url}     ||= 'https://cloudpt.pt/oauth/access_token',
+  if ( $self->{key} and $self->{secret} )
+  {
+    $self->{ua} ||= LWP::UserAgent->new;
 
-  return $self;
+    $self->{callback_url}   ||= 'oob',
+    $self->{request_url}    ||= 'https://cloudpt.pt/oauth/request_token',
+    $self->{authorize_url}  ||= 'https://cloudpt.pt/oauth/authorize',
+    $self->{access_url}     ||= 'https://cloudpt.pt/oauth/access_token',
+
+    $self->{api_endpoint}     ||= 'https://publicapi.cloudpt.pt';
+    $self->{content_endpoint} ||= 'https://api-content.cloudpt.pt';
+
+    return $self;
+  }
+  else
+  {
+    carp "ERROR: Required parameters missing.";
+  }
 }
+
+=head2 login
+
+Perform the initial login operation, identifying the client on the service.
+
+If the handshake is successful, a request token/secret is obtained which allows
+an authorization URL to be returned. This URL must be opened by the user to
+explicitly authorize access to the service's account.
+
+Furthermore, CloudPT then either redirects the user back to the callback URL
+(if defined in C<$self-E<gt>{callback_url}>), or openly provides a PIN number
+that will be required to verify that the user's authorization is valid.
+
+=cut
 
 sub login
 {
@@ -77,11 +134,20 @@ sub login
   }
 }
 
-sub auth
+=head2 authorize
+
+This method exchanges the request token/secret, obtained after a successful
+login, with an access token/secret that is needed for subsequent accesses to
+the service's API.
+
+The C<verifier> PIN parameter is required.
+
+=cut
+
+sub authorize
 {
   my $self = shift;
-
-  my %args = @_; %args = map { lc $_ => $args{$_} } keys %args;
+  my %args = $self->_normalize_args( @_ );
 
   if ( $args{verifier} )
   {
@@ -126,28 +192,45 @@ sub auth
   }
 }
 
-=head1 NAME
+=head1 INTERNAL API
 
-Net::CloudPT - A CloudPT interface
+=head2 _nonce
 
-=head1 SYNOPSYS
+Generate a unique 'nonce' to be used on each request.
 
-This module is a Perl interface to the API for the Portuguese cloud storage
-service CloudPT. You can learn more about it at L<http://www.cloudpt.pt>.
+=cut
 
-Quick start:
+sub _nonce { join( '', rand_chars( size => 16, set => 'alphanumeric' )); }
 
-    use Net::CloudPT;
+=head2 _normalize_args
 
-    my $cloud = Net::CloudPT->new( key => 'KEY', secret => 'SECRET' );
-    $cloud->login;
+Simple convenience method that lower-cases all argument names.
 
-    # authorize the app, retrieving the verifier PIN...
+=cut
 
-    $cloud->auth( verifier => $pin );
+sub _normalize_args
+{
+  my $class = shift;
+  my %args = @_;
 
-The particular details regarding the API can be found at L<https://cloudpt.pt/documentation>
+  return map { lc $_ => $args{$_} } keys %args;
+}
+
+=head1 AUTHOR
+
+Sérgio Bernardino, C<< <code@sergiobernardino.net> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Sérgio Bernardino.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
 1;
+
